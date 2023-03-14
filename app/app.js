98600ac95477b7e198443037d9b13f0009bcdb28
MartynasDruteika <martynas.druteika@devbridge.com>
2023-03-14 18:59:32 +0200

    Move endpoints into separate routers

--- app/app.js
+++ app/app.js
@@ -1,57 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
-const { tasksManagerConnection, codeacademyConnection } = require('./db');
+const tasksRouter = require('./router/tasks');
+const assignmentsRouter = require('./router/assignments');
 
 const app = express();
 
 app.use(cors());
-
-app.get('/tasks', (req, res) => {
-    tasksManagerConnection.execute('SELECT * FROM tasks', (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }
-    });
-});
-
-app.get('/assignments', (req, res) => {
-    const { done } = req.query;
-
-    if (done === '0' || done === '1') {
-        codeacademyConnection.execute(
-            'SELECT * FROM assignments WHERE done=?',
-            [done],
-            (err, result) => {
-                if (err) {
-                    res.json(err);
-                } else {
-                    res.json(result);
-                }
-            }
-        )
-    } else {
-        codeacademyConnection.execute('SELECT * FROM assignments', (err, result) => {
-            if (err) {
-                res.json(err);
-            } else {
-                res.json(result);
-            }
-        });
-    }
-});
-
-app.get('/assignments/done', (req, res) => {
-    codeacademyConnection.execute('SELECT * FROM assignments WHERE done=1', (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }
-    });
-});
+app.use(tasksRouter);
+app.use(assignmentsRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
