2c3c79336f1b5cdf1fc706940cfa680db02cf7ec
MartynasDruteika <martynas.druteika@devbridge.com>
2023-03-14 18:26:08 +0200

    Mysql #2 paskaita sprendimas

--- app/app.js
+++ app/app.js
@@ -14,10 +14,55 @@ const mysqlConfig = {
     port: 3306
 }
 
+const mysqlConfig2 = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'test123',
+    database: 'codeacademy',
+    port: 3306
+}
+
 const connection = mysql.createConnection(mysqlConfig);
+const connection2 = mysql.createConnection(mysqlConfig2);
 
 app.get('/tasks', (req, res) => {
     connection.execute('SELECT * FROM tasks', (err, result) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.get('/assignments', (req, res) => {
+    const { done } = req.query;
+
+    if (done === '0' || done === '1') {
+        connection2.execute(
+            'SELECT * FROM assignments WHERE done=?',
+            [done],
+            (err, result) => {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(result);
+                }
+            }
+        )
+    } else {
+        connection2.execute('SELECT * FROM assignments', (err, result) => {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(result);
+            }
+        });
+    }
+});
+
+app.get('/assignments/done', (req, res) => {
+    connection2.execute('SELECT * FROM assignments WHERE done=1', (err, result) => {
         if (err) {
             res.json(err);
         } else {
