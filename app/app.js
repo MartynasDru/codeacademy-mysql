fc8fce0fa51cd6c70700d98ba86fc99d22de5d14
MartynasDruteika <martynas.druteika@devbridge.com>
2023-03-14 18:41:48 +0200

    Extract db connection logic into db.js

--- app/app.js
+++ app/app.js
@@ -1,32 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2');
+
+const { tasksManagerConnection, codeacademyConnection } = require('./db');
 
 const app = express();
 
 app.use(cors());
 
-const mysqlConfig = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'test123',
-    database: 'tasks_manager',
-    port: 3306
-}
-
-const mysqlConfig2 = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'test123',
-    database: 'codeacademy',
-    port: 3306
-}
-
-const connection = mysql.createConnection(mysqlConfig);
-const connection2 = mysql.createConnection(mysqlConfig2);
-
 app.get('/tasks', (req, res) => {
-    connection.execute('SELECT * FROM tasks', (err, result) => {
+    tasksManagerConnection.execute('SELECT * FROM tasks', (err, result) => {
         if (err) {
             res.json(err);
         } else {
@@ -39,7 +21,7 @@ app.get('/assignments', (req, res) => {
     const { done } = req.query;
 
     if (done === '0' || done === '1') {
-        connection2.execute(
+        codeacademyConnection.execute(
             'SELECT * FROM assignments WHERE done=?',
             [done],
             (err, result) => {
@@ -51,7 +33,7 @@ app.get('/assignments', (req, res) => {
             }
         )
     } else {
-        connection2.execute('SELECT * FROM assignments', (err, result) => {
+        codeacademyConnection.execute('SELECT * FROM assignments', (err, result) => {
             if (err) {
                 res.json(err);
             } else {
@@ -62,7 +44,7 @@ app.get('/assignments', (req, res) => {
 });
 
 app.get('/assignments/done', (req, res) => {
-    connection2.execute('SELECT * FROM assignments WHERE done=1', (err, result) => {
+    codeacademyConnection.execute('SELECT * FROM assignments WHERE done=1', (err, result) => {
         if (err) {
             res.json(err);
         } else {
