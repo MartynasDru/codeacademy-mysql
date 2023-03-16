ca9cec08f2ad524a1d0a805224e8749612068b2b
MartynasDruteika <martynas.druteika@devbridge.com>
2023-03-16 18:44:28 +0200

    tasks router improvements

--- app/router/tasks.js
+++ app/router/tasks.js
@@ -6,9 +6,19 @@ const router = express.Router();
 
 router.get('/tasks', (req, res) => {
     tasksManagerConnection.execute(
-        'SELECT * FROM tasks', 
+        'SELECT tasks.id, tasks.name, users.name as user_name, users.email FROM tasks LEFT JOIN users ON tasks.user_id = users.id',
         (err, result) => defaultCallback(err, result, res)
     );
+});
+
+router.get('/tasks/:id', (req, res) => {
+    const { id } = req.params;
+
+    tasksManagerConnection.execute(
+        'SELECT tasks.id, tasks.name, users.name as user_name, users.email FROM tasks LEFT JOIN users ON tasks.user_id = users.id WHERE tasks.id=?',
+        [id],
+        (err, result) => defaultCallback(err, result, res)
+    )
 });
 
 module.exports = router;
